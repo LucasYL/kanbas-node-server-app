38d8bf97a4d9618c4efac816387bc3a46628464e
luyingcong617@gmail.com <luyingcong617@gmail.com>
2024-08-12 11:22:42 -0700

    Fixed Errors

--- Courses/dao.js
+++ Courses/dao.js
@@ -1,10 +1,21 @@
 import CourseModel from "./model.js";
 
-export const createCourse = (course) => CourseModel.create(course);
-export const findAllCourses = () => CourseModel.find();
-export const findCourseById = (courseId) => CourseModel.findById(courseId);
-export const updateCourse = (courseId, course) => 
-  CourseModel.updateOne({ _id: courseId }, { $set: course });
-export const deleteCourse = (courseId) => 
-  CourseModel.deleteOne({ _id: courseId });
-export const findCourseByNumber = (number) => model.findOne({ number });
+export const createCourse = async (course) => {
+    return await CourseModel.create(course);
+};
+
+export const findAllCourses = async () => {
+    return await CourseModel.find();
+};
+
+export const findCourseById = async (courseId) => {
+    return await CourseModel.findById(courseId);
+};
+
+export const updateCourse = async (courseId, course) => {
+    return await CourseModel.updateOne({ _id: courseId }, { $set: course });
+};
+
+export const deleteCourse = async (courseId) => {
+    return await CourseModel.deleteOne({ _id: courseId });
+};
